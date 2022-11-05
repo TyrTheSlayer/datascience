a3eaa4d1b5e5ee5c9a0df0268dc7852671c7d596
tyr <aedan.wells@student.nmt.edu>
2022-11-05 12:07:02 -0600

    put gathering and appending code

--- CSE Class/final_project/clean_combine.py
+++
@@ -1,8 +0,0 @@
-#import the modules
-import os
-import pandas as pd
-#read the path
-goalie_path = "~/datascience/cse441/final_project/raw_21_goalies"
-#list all the files from the directory
-file_list = os.listdir(goalie_path)
-file_list
--- cse441/final_project/clean_combine.py
+++ cse441/final_project/clean_combine.py
@@ -1,8 +1,26 @@
-#import the modules
+#clean_combine.py
+#Author Aedan Wells
+#date 11/5/2022
+
 import os
 import pandas as pd
 #read the path
-file_path = "~/datascience/CSE Class/final_project/"
+goalie_path = "raw_21_goalies/"
+skater_path = "raw_21_skaters/"
 #list all the files from the directory
-file_list = os.listdir(file_path)
-file_list
+goalie_file_list = os.listdir(goalie_path)
+skater_file_list = os.listdir(skater_path)
+
+#get all of the goalie files together
+for file in goalie_file_list:
+    temp_frame = pd.read_csv(file)
+    df_append = df_append.append(temp_frame, ignore_index=True)
+
+df_append.to_csv('raw_21_goalies/full_goalies.csv')
+
+#get all of the skater files together
+for file in skater_file_list:
+    temp_frame = pd.read_csv(file)
+    frame_append = frame_append.append(temp_frame, ignore_index=True)
+
+frame_append.to_csv('raw_21_skaters/full_skaters.csv')

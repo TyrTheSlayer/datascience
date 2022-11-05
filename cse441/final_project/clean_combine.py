10625c3041573f0eb2c8c07500257f8c9ad0cd8e
tyr <aedan.wells@student.nmt.edu>
2022-11-05 11:44:16 -0600

    add final project files

---
+++ cse441/final_project/clean_combine.py
@@ -0,0 +1,8 @@
+#import the modules
+import os
+import pandas as pd
+#read the path
+file_path = "~/datascience/CSE Class/final_project/"
+#list all the files from the directory
+file_list = os.listdir(file_path)
+file_list

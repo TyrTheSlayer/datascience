9f51a06bd52082a34a01f5fe3a78505bfc14d78e
tyr <aedan.wells@student.nmt.edu>
2022-11-06 12:52:14 -0700

    Run and get full csv's

--- cse441/final_project/clean_combine.py
+++ cse441/final_project/clean_combine.py
@@ -11,18 +11,20 @@ skater_path = "raw_21_skaters/"
 goalie_file_list = os.listdir(goalie_path)
 skater_file_list = os.listdir(skater_path)
 
+df_append = pd.DataFrame()
 #get all of the goalie files together
 for file in goalie_file_list:
     temp_frame = pd.read_csv(goalie_path + "/" +file)
-    temp_main = temp_frame[temp_frame['situation' == 'all']]
+    temp_main = pd.DataFrame(temp_frame[temp_frame['situation'] == 'all'])
     df_append = df_append.append(temp_frame, ignore_index=True)
 
 df_append.to_csv('raw_21_goalies/full_goalies.csv')
 
+frame_append = pd.DataFrame()
 #get all of the skater files together
 for file in skater_file_list:
     temp_frame = pd.read_csv(skater_path + "/" +file)
-    temp_main = temp_frame[temp_frame['situation' == 'all']]
+    temp_main = pd.DataFrame(temp_frame[temp_frame['situation'] == 'all'])
     frame_append = frame_append.append(temp_frame, ignore_index=True)
 
 frame_append.to_csv('raw_21_skaters/full_skaters.csv')
